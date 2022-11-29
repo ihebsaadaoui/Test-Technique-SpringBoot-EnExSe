1e62c561ecde7efaf739ed53984f73e3230082d0
ihebsaadaoui <iheb.saadaoui@esprit.tn>
2022-11-29 23:19:19 +0100

    Initial commit

---
+++ src/main/java/com/iheb/test/TestTechniqueSpringBootEnExSeApplication.java
@@ -0,0 +1,13 @@
+package com.iheb.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTechniqueSpringBootEnExSeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTechniqueSpringBootEnExSeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iheb/test/TestTechniqueSpringBootEnExSeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iheb.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTechniqueSpringBootEnExSeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
